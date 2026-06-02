63a9554e6ee64f09cb07c9957dbe70b1a0cd3c70
Yuvraj Gupta <ygyuvrajgupta2007@gmail.com>
2026-06-02 16:59:45 +0000

    boolean

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,8 +1,8 @@
 "use strict"; //Treats all codes as it written in newr version..//  node 01_basics/02_dataTypes.js
-alert (3+3)  // we are using node.js not browser..
+// alert (3+3)  // we are using node.js not browser..
 console.log("Yuvraj")
 
 let name = "Yuvraj"             //string
 let age = 18                    //numbers
 let isloggedin = false          //boolean
-console.log(typeof age)
+console.log(typeof "Yuvraj")
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33abc"                     //node 01_basics/03_conversionOperation.js
+//console.log(typeof score);
+//console.log(typeof (score));
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+let a=1
+let b= Boolean(a);
+console.log(a, "is coverted to Boolean =",b)
+
+let IsLoggedIn=1
+let BooleanIsLoggedIn= Boolean(IsLoggedIn);
+console.log(IsLoggedIn, "is coverted to Boolean =",BooleanIsLoggedIn)
+//1 => true; 0 => false; 5 => true
+//"" => false; "halo" => true
