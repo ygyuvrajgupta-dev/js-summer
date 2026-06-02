ec5138ab95a48fb5d545d75b7cc781854bcef18f
Yuvraj Gupta <ygyuvrajgupta2007@gmail.com>
2026-06-02 15:22:36 +0000

    uploaded

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 11223344                      //node 01_basics/01_variables.js
+let accountEmail = "1234@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId=2 //not allowed
+accountEmail = "hc@hc"
+accountPassword = "12276"
+accountCity = "Kanpur"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity ])
