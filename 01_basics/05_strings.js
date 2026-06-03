4c9a3eba038c69199713d6d6036f474f9857ef75
Yuvraj Gupta <ygyuvrajgupta2007@gmail.com>
2026-06-03 15:24:54 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,3 +1,25 @@
+const name = "Yuvraj Gupta"
+const repo = 9
+// console.log(name,repo,"Thanks");
+// console.log(`My name is ${name} and my repo count is ${repo}`);
+const newKr = new String ('Mangalam') 
+// console.log(newKr)
+// console.log(newKr[0]);
+// console.log(newKr.length);
+// console.log(newKr.toUpperCase());
+// console.log(newKr.toLowerCase());
+// console.log(newKr.charAt(3));
+// console.log(newKr.indexOf('a'));
+const newString = newKr;
+// console.log(newString.substring(0,4));
+// console.log(newString.slice(-7,4));
+//######################################################################################################################
+//                                            Space Handling
+const NewString = "   Yuvraj   "
+// console.log(NewString)
+// console.log(NewString.trim())
 
-
+const String1 = "everywhere"
+let a = String1.replace('e','f')
+console.log(a)
 //node 01_basics/05_strings.js
