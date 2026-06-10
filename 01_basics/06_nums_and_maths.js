581eafc83421225586a4670a8ad2a90fe0052289
Yuvraj Gupta <ygyuvrajgupta2007@gmail.com>
2026-06-10 16:50:26 +0530

    num

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -9,8 +9,8 @@
 // console.log(Math.round(4.6 ));
 // console.log(Math.ceil(4.1 ));
 // console.log(Math.floor(4.6 ));
-console.log(Math.random());
-console.log((Math.random()*10)+1);
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
 
-
+console.log(Math.floor(Math.random() * (max - min +1))+min)
 // node 01_basics/06_nums_and_maths.js
