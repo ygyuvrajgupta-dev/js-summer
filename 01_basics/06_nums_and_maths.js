21c69cad20e517935b7ee6eee3e5cdd4a3280ac5
Yuvraj Gupta <ygyuvrajgupta2007@gmail.com>
2026-06-10 15:48:31 +0530

    maths start

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,6 +1,16 @@
-const score = 400
-console.log(score)
-const num = new Number(400)
-console.log(num)
+// const score = 400
+// console.log(score)
+// const num = new Number(400)
+// console.log(num)
+
+//+++++++++++++++++++++Maths++++++++++++++++++++++++++++++
+// console.log("Maths");
+// console.log(Math.abs(-4 ));
+// console.log(Math.round(4.6 ));
+// console.log(Math.ceil(4.1 ));
+// console.log(Math.floor(4.6 ));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
 
 // node 01_basics/06_nums_and_maths.js
